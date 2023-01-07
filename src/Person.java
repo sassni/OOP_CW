0f149a3c527685c295c6d9f42dd3a54d8bdf8df4
Sasni MRM <sasni.20200168@iit.ac.lk>
2023-01-07 17:10:11 +0530

    Person added

---
+++ src/Person.java
@@ -0,0 +1,11 @@
+public class Person {
+    private String name, surname, dob;
+    private int mobileNumber;
+
+    public Person(String name, String surname, String dob, int mobileNumber) {
+        this.name = name;
+        this.surname = surname;
+        this.dob = dob;
+        this.mobileNumber = mobileNumber;
+    }
+}
